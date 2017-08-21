f6da6588eb4a7b852555991a50fd3d0dfeda83de
Nate Rickard <nate.rickard@microsoft.com>
2017-08-21 15:16:07 -0400

    added application class and cleaned up

--- Producer/Producer.Droid/MainActivity.cs
+++ Producer/Producer.Droid/MainActivity.cs
@@ -7,8 +7,6 @@ namespace Producer.Droid
 	[Activity (Label = "Producer", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
@@ -17,12 +15,6 @@ namespace Producer.Droid
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
-
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
 		}
 	}
 }
---
+++ Producer/Producer.Droid/ProducerApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace Producer.Droid
+{
+	[Application]
+	public class ProducerApp : Application
+	{
+		/// <summary>
+		/// Base constructor which must be implemented if it is to successfully inherit from the Application class.
+		/// </summary>
+		/// <param name="handle"></param>
+		/// <param name="transfer"></param>
+		public ProducerApp (IntPtr handle, JniHandleOwnership transfer)
+			: base (handle, transfer)
+		{
+		}
+
+
+		public override void OnCreate ()
+		{
+			base.OnCreate ();
+
+			//JsonHttpClient.GlobalHttpMessageHandlerFactory = () => new NativeMessageHandler ();
+			//JsConfig.PropertyConvention = PropertyConvention.Lenient;
+		}
+
+	}
+}
