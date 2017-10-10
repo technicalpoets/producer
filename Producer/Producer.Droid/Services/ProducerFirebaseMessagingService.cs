b0010f58b2cb796db798d6f4bf0d6c81cef92f2d
Nate Rickard <nate.rickard@microsoft.com>
2017-10-10 14:44:22 -0400

    added stubbed DeleteLocalUploads() code

--- Producer/Producer.Droid/Services/ProducerFirebaseMessagingService.cs
+++ Producer/Producer.Droid/Services/ProducerFirebaseMessagingService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using Android.App;
 using Firebase.Messaging;
+using Producer.Domain;
 using Producer.Shared;
 
 namespace Producer.Droid.Services
@@ -34,7 +36,7 @@ namespace Producer.Droid.Services
 				//refresh content - this will generate an AvContentChanged event the UI will pick up
 				await ContentClient.Shared.GetAllAvContent ();
 
-				//DeleteLocalUploads ();
+				DeleteLocalUploads ();
 
 				Log.Debug ($"Finished Getting Data.");
 			}
@@ -45,6 +47,33 @@ namespace Producer.Droid.Services
 			finally
 			{
 				processingNotification = false;
+			}
+		}
+
+
+		void DeleteLocalUploads ()
+		{
+			var locals = ContentClient.Shared.AvContent? [UserRoles.Producer].Where (avc => avc.HasLocalInboxPath);
+
+			foreach (var asset in locals)
+			{
+				Log.Debug ($"Deleting local asset at: {asset.LocalInboxPath}");
+
+				//NSFileManager.DefaultManager.Remove (asset.LocalInboxPath, out NSError error);
+
+				//if (error == null)
+				//{
+				//	asset.LocalInboxPath = null;
+				//}
+				//else
+				//{
+				//	if (error.Code == 4) // not found
+				//	{
+				//		asset.LocalInboxPath = null;
+				//	}
+
+				//	Log.Error ($"{error}\n{error.Description}");
+				//}
 			}
 		}
 	}
